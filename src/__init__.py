1756147a5768eac8461f819825f4e10b42f79139
iamsvp94 <PSV1994@yandex.ru>
2024-08-25 14:13:33 +0500

    added funcs in init

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,9 @@ import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.resolve()))
+from src.utils_pl import CustomDataset, Classifier_pl, MetricSMPCallback
 from src.constants import BASE_DIR, AVAIL_GPUS, NUM_WORKERS, SEED
 from src.utils import glob_search, plt_show_img
-from src.utils_pl import CustomDataset, Classifier_pl
-from src.models import EyeClassifier
+from src.models import CustomNet
 
 # TODO: add __all__ = []
